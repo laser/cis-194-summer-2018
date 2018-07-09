6cfa965e8db0b08b0474d06d458ff9024fa2161d
Erin Swenson-Healey <l@s3r.me>
2018-07-09 16:25:05 -0700

    add week 1 solution from 2016

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,30 +2,41 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n | n < 1 = []
+           | otherwise = toDigits (dropLastDigit n) ++ [lastDigit n]
+  where
+    lastDigit :: Integer -> Integer
+    lastDigit n = mod n 10
+
+    dropLastDigit :: Integer -> Integer
+    dropLastDigit n = div n 10
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = reverse . toDigits
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther xs = snd $ foldr (\x (term, acc) ->
+  ((if term == 2 then 1 else 2), ((x * term) : acc))) (1, []) xs
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits [] = 0
+sumDigits (x:xs) = sum (toDigits x) + sumDigits xs
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = 0 == sumDigits (doubleEveryOther (toDigits n)) `mod` 10
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _    = []
+hanoi 1 p1 p2 _  = [(p1, p2)]
+hanoi n p1 p2 p3 = (hanoi (n-1) p1 p3 p2) ++ [(p1, p2)] ++ (hanoi (n-1) p3 p2 p1)
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
