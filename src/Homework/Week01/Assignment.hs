83a4e78220503822b2f6b5b05b2b542e6099181f
Alex Mouton <thomas.mouton@gmail.com>
2018-07-11 23:09:42 -0700

    Week 1 assignment.

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -1,31 +1,45 @@
 module Homework.Week01.Assignment where
 
+import Data.Monoid
+
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits x
+  | x <= 0 = []
+  | x > 0 = (toDigits (div x 10)) <> [mod x 10]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev x
+  | x <= 0 = []
+  | x > 0 = [mod x 10] <> (toDigitsRev (div x 10))
 
--- #2
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+-- #
+doubleEveryOther = reverse . doubleEveryOther' . reverse
+
+doubleEveryOther' :: [Integer] -> [Integer]
+doubleEveryOther' [] = []
+doubleEveryOther' [x] = [x]
+doubleEveryOther' (x:y:rest) = [x, 2*y] <> doubleEveryOther' rest
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits = foldl (+) 0 . (=<<) toDigits
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate = (==) 0 . ((flip mod) 10) . checksum
+
+checksum :: Integer -> Integer
+checksum = sumDigits . doubleEveryOther. toDigits
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 _ _ _ = []
+hanoi i source dest temp = hanoi (i-1) source temp dest <> [(source, dest)] <> hanoi (i-1) temp dest source
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
