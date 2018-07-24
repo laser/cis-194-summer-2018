2a08286196ada1691caf67f8eb300ad617faaeb0
Tim Kersey <t@k-t.im>
2018-07-23 17:39:13 -0700

    Week 01

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -1,31 +1,50 @@
 module Homework.Week01.Assignment where
+import Data.List 
+import Data.Ord
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n
+  | n <= 0 = []
+  | otherwise = map (\n -> read [n]) (show n)
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev = reverse . toDigits
 
 -- #2
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft n = zipWith (*) n (cycle [1,2])
+
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther = reverse . doubleEveryOtherFromLeft . reverse
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+-- sumDigits n = foldl (+) 0 (concatMap toDigits n)
+sumDigits = sum . concat . map toDigits
+
 
 -- #4
+isDivisibleByTen :: Integer -> Bool
+isDivisibleByTen x = x `rem` 10 == 0
+
 validate :: Integer -> Bool
-validate = undefined
+validate = isDivisibleByTen . sumDigits . doubleEveryOther . toDigits
 
 -- #5
 type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 0 a b c = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+
+shortest :: [[a]] -> [a]
+shortest [] = []
+shortest ls = minimumBy (comparing length) ls
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
-hanoi4 = undefined
+hanoi4 0 a b c d = [ ]
+hanoi4 1 a b c d = [(a, b)]
+hanoi4 n a b c d = shortest [(hanoi4 (n - k) a d c b ++ hanoi k a b c ++ hanoi4 (n - k) d b a c) | k <- [1..(n-1)]]
