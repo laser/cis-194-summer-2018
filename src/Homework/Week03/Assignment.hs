5952920b92eee26463ff64e321918d352dab60dd
Solomon Bothwell <ssbothwell@gmail.com>
2018-08-02 17:49:01 -0700

    Finishes week6

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -5,6 +5,15 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' f b [] = b
+foldl' f b (x:xs) = foldl' f (f b x) xs
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' f b [] = b
+foldr' f b (x:xs) = f x (foldr' f b xs)
+
 -- #1
 skips :: [a] -> [[a]]
 skips [] = []
@@ -25,31 +34,24 @@ localMaxima xs = go xs []
 
 -- #3
 histogram :: [Integer] -> String
-histogram = histoRows
-
-histo :: [Integer] -> [Integer]
-histo xs = go xs [] 0
-    where go _ zs 10 = zs
-          go ys zs i = go ys (zs++[fromIntegral $ length $ filter (\x -> x == i) ys]) $ i+1
-
-res = [0,3,0,0,0,1,0,0,0,0]
-
-join char []              =  ""
-join char (w:ws)          = w ++ go ws
-  where
-    go []     = ""
-    go (v:vs) = char : (v ++ go vs)
-
-
-histoRow :: [Integer] -> ([Integer], String)
-histoRow xs = (map (\x -> if x == 0 then 0 else x-1) xs,
-               map (\x -> if x == 0 then ' ' else '*') xs) 
-
-histoRows :: [Integer] -> String
-histoRows xs = go (histo xs) [] $ maximum $ histo xs 
+histogram xs = go (histo xs) [] $ maximum $ histo xs 
     where i = maximum xs
           go _ ys 0 = join '\n' (ys++["==========\n0123456789\n"])
           go xs ys i = 
             let (ints, str) = histoRow xs
             in go ints (str:ys) (i-1)
 
+histo :: [Integer] -> [Integer]
+histo xs = go xs [] 0
+    where go _ zs 10 = zs
+          go ys zs i = go ys (zs++[fromIntegral $ length $ filter (\x -> x == i) ys]) $ i+1
+
+join char [] = ""
+join char (w:ws) = w ++ go ws
+  where
+    go [] = ""
+    go (v:vs) = char : (v ++ go vs)
+
+histoRow :: [Integer] -> ([Integer], String)
+histoRow xs = (map (\x -> if x == 0 then 0 else x-1) xs,
+               map (\x -> if x == 0 then ' ' else '*') xs) 
--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -13,37 +13,67 @@ module Homework.Week06.Assignment (
 
 -- #1a
 fib :: Integer -> Integer
-fib = undefined
+fib n = f 0 1 0
+    where f n1 n2 i = if i == n then n1 else f n2 (n1+n2) (i+1)
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = [ fib n | n <- [0..]]
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Cons a (Stream a) deriving (Show)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons a b) = a:(streamToList b)
 
--- instance Show a => Show (Stream a) where
---   show = ???
+--instance Show a => Show (Stream a) where
+--   show = undefined
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat a = Cons a (streamRepeat a)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons a b) = Cons (f a) (streamMap f (b))
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f a = Cons a (streamFromSeed f (f a))
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
+
+{-
+
+1 2 1 3 1  2  1  4  1  3  1  3
+2 4 6 8 10 12 14 16 18 20 22 24
+
+p 0 1 0 2 0 1 0 3 0 1  0  2  0  1  0  4  0  1  0  2  0  1  0  3
+n 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+
+-}
+
+log2 :: Integer -> Integer
+log2 = truncate . logBase 2 . fromInteger
+
+powersOfTwo :: [Integer]
+powersOfTwo = [2^x | x <- [1..]]
+
+maxPower :: Integer -> Integer
+maxPower x = log2 . last . filter (\y  -> x `mod` y == 0) $ takeWhile (<= x) $ powersOfTwo
+
+evens :: Stream Integer
+evens = streamMap maxPower (streamFromSeed (+2) 2)
+
+odds :: Stream Integer
+odds = streamRepeat 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x y) (Cons x' y') =
+    Cons x (Cons x' (interleaveStreams y y'))
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = interleaveStreams odds evens
