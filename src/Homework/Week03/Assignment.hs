90c0cf4ce42e041b41f13a95571fd1b81267709a
rashadg1030 <rashad.sasaki@gmail.com>
2018-08-08 17:36:01 -0700

    Completed problem #2.

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,14 +4,36 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Data.List
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips [] = []
+skips [x] = [x] : []
+skips x = x : []
+skips all@(x:xs) = (x : []) : [] 
+
 
 -- #2
+-- 173 characters 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_,_] = []
+localMaxima a@(l:m:r:t) = if m > l && m > r then m : (localMaxima $ drop 1 a) else localMaxima $ drop 1 a
 
+-- 148 characters 
+{-
+localMaxima :: [Integer] -> [Integer]
+localMaxima a@(l:m:r:t)
+  | length a < 3 = []
+  | otherwise = if m > l && m > r then m : (localMaxima $ drop 1 a) else localMaxima $ drop 1 a
+-}
+ 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram [] = "\n==========\n0123456789\n"
+
+test :: [Integer] -> IO()
+test x = putStr "*\n**\n==========\n0123456789\n"
+test x = putStr $ histogram x
