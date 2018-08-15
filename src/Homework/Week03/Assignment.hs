ccef2ba07afb4bbc4fd73b6f2abeb5bbbf61fddd
Solomon Bothwell <ssbothwell@gmail.com>
2018-08-14 19:51:40 -0700

    foo

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -5,24 +5,41 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Data.List
 
-foldl' :: (b -> a -> b) -> b -> [a] -> b
-foldl' f b [] = b
-foldl' f b (x:xs) = foldl' f (f b x) xs
 
-foldr' :: (a -> b -> b) -> b -> [a] -> b
-foldr' f b [] = b
-foldr' f b (x:xs) = f x (foldr' f b xs)
+--foldl' :: (b -> a -> b) -> b -> [a] -> b
+--foldl' f b [] = b
+--foldl' f b (x:xs) = foldl' f (f b x) xs
+--
+--foldr' :: (a -> b -> b) -> b -> [a] -> b
+--foldr' f b [] = b
+--foldr' f b (x:xs) = f x (foldr' f b xs)
 
 -- #1
-skips :: [a] -> [[a]]
-skips [] = []
-skips str = map (skips' str) [0..(length str - 1)]
 
+skips'' :: [a] -> [[a]]
+skips'' [] = []
+skips'' str = map (skips' str) [0..(length str - 1)]
+
+skips' :: [a] -> Int -> [a]
 skips' xs i = go i (drop i xs) []
     where go _ [] ys = ys
           go i' (x:xs') ys = go i' (drop i' xs') (ys++[x])
 
+-- 1) Make a list lists starting from n to the length of the list
+-- incremented by 2*n where n is a number from 1 to the length of the list. 
+--   eg, length 5 = [[1,2,3,4,5], [2, 4], [3], [4], [5]]
+-- 2) map those numbers to the input string as indexes.
+skips :: [a] -> [[a]]
+skips xs = let l = length xs in
+    (fmap . fmap) (\i -> xs !! (i-1)) [[n, n*2..l] | n <- [1..l]]
+
+
+-- 1) recursively pattern match on the input with (x:y:[]) 
+--    as the base case returning the accumulator list ys.
+-- 2) if y is a local maxima then concat is onto the accumulator
+--    and recurse on the tail.
 -- #2
 localMaxima :: [Integer] -> [Integer]
 localMaxima xs = go xs [] 
@@ -36,7 +53,7 @@ localMaxima xs = go xs []
 histogram :: [Integer] -> String
 histogram xs = go (histo xs) [] $ maximum $ histo xs 
     where i = maximum xs
-          go _ ys 0 = join '\n' (ys++["==========\n0123456789\n"])
+          go _ ys 0 = intercalate "\n" (ys++["==========\n0123456789\n"])
           go xs ys i = 
             let (ints, str) = histoRow xs
             in go ints (str:ys) (i-1)
@@ -45,12 +62,6 @@ histo :: [Integer] -> [Integer]
 histo xs = go xs [] 0
     where go _ zs 10 = zs
           go ys zs i = go ys (zs++[fromIntegral $ length $ filter (\x -> x == i) ys]) $ i+1
-
-join char [] = ""
-join char (w:ws) = w ++ go ws
-  where
-    go [] = ""
-    go (v:vs) = char : (v ++ go vs)
 
 histoRow :: [Integer] -> ([Integer], String)
 histoRow xs = (map (\x -> if x == 0 then 0 else x-1) xs,
