03f647ecd50a5fce06178c8895c7eef7f0a458cb
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2018-08-27 12:02:51 -0700

    Finished week4 problems

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -20,26 +20,29 @@ module Homework.Week04.Assignment (
 ) where
 
 import Homework.Week04.BST
+import Data.Char
+import Data.Maybe
+import Data.List
 
 -- #1
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 int bool = bool
 
 -- #2
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 int int2 = int2
 
 -- #3
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 int a = a
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 bool a a2 = a
 
 -- #5
 ex5 :: Bool -> Bool
-ex5 = undefined
+ex5 bool = bool
 
 -- #6
 ex6 :: (a -> a) -> a
@@ -47,44 +50,61 @@ ex6 = undefined
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 f a = a
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 = id
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+ex9 = map
 
 -- #10
 ex10 :: Maybe a -> a
-ex10 = undefined
+ex10 = fromJust
+
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 = return
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+ex12 = id
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ val Leaf = Node Leaf val Leaf
+insertBST comp val node@(Node left x right)
+  | compVal == GT = insertBST comp val right
+  | compVal == LT = insertBST comp val left
+  | compVal == EQ = node
+    where compVal = comp val x
 
 -- #14
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps [] = True
+allCaps (x:xs) =
+  case safeHead x of
+    Just h -> case isUpper h of
+      True -> allCaps xs
+      False -> False
+    Nothing -> False
+
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:xs) = Just x
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+dropTrailingWhitespace str = reverse $ dropWhile (\a -> a == ' ') $ reverse str
 
 -- #16
 firstLetters :: [String] -> [Char]
-firstLetters = undefined
+firstLetters strings = catMaybes $ map safeHead $ strings
 
 -- #17
 asList :: [String] -> String
-asList = undefined
+asList strings = "[" ++ concat (intersperse "," ( filter (not . null) strings)) ++ "]"
