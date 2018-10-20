38255204e3ae30d9962ef6ca1f9448acb08d4fd8
Solomon Bothwell <ssbothwell@gmail.com>
2018-10-19 19:22:22 -0700

    completes week 8

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -8,34 +8,92 @@ module Homework.Week08.Assignment (
 
 import Homework.Week08.AParser
 import Control.Applicative
+import Data.Char
+
+
+newtype P a = P { runParser' :: String -> Maybe (a, String) }
 
 -- #1
 first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+first f (a, b) = (f a, b) 
+
+first' :: (a -> b) -> (c,a) -> (c,b)
+first' = fmap 
 
 instance Functor Parser where
-  fmap = undefined
+  fmap f pa = Parser $ \s -> do
+    (a, xs)  <- runParser pa $ s
+    return (f a, xs)
 
 -- #2
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure a = Parser $ \s -> Just (a, s)
+  pab <*> pa = Parser $ \s -> do
+    (a, xs) <- runParser pa $ s
+    (ab', ys) <- runParser pab $ xs
+    return (ab' a, ys)
 
+instance Monad Parser where
+    return = pure
+    (>>=) p f = Parser $ \s -> do
+        (a, xs) <- runParser p s
+        (b, ys) <- runParser (f a) xs
+        return (b, ys)
+
+        
 -- #3
+safeHead :: [a] -> Maybe (a, [a])
+safeHead [] = Nothing
+safeHead (x:xs) = Just (x, xs)
+
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = do
+    a <- char 'a'
+    b <- char 'b'
+    return (a, b)
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = do
+    a <- char 'a'
+    b <- char 'b'
+    return ()
 
 intPair :: Parser [Integer]
-intPair = undefined
+intPair = do
+    a <- posInt
+    char ' '
+    b <- posInt
+    return [a, b]
 
 -- #4
+
+failure :: Parser a
+failure = Parser $ \s -> Nothing
+
+combine :: Parser a -> Parser a -> Parser a
+combine pa pb = Parser $ \s ->
+    case runParser pa $ s of
+        Just (a, xs) -> Just (a, xs)
+        Nothing -> case runParser pb $ s of
+            Just (b, ys) -> Just (b, ys)
+            Nothing -> Nothing
+
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = failure
+  a <|> b = combine a b
 
 -- #5
+parseUpper :: Parser ()
+parseUpper = do
+    satisfy isUpper
+    return ()
+
+parseInt :: Parser ()
+parseInt = do
+    posInt
+    return ()
+
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = parseUpper <|> parseInt
+    
+    
--- test/Homework/Week08Spec.hs
+++ test/Homework/Week08Spec.hs
@@ -20,7 +20,7 @@ spec = do
   describe "Functor Parser" $ do
     describe "fmap" $ do
       it "applies a function to the result of a parser" $ do
-        pending
+        --pending
         let p = fmap (+1) posInt
         runParser p "41" `shouldBe` Just (42, "")
         runParser p "x"  `shouldBe` Nothing
@@ -28,12 +28,12 @@ spec = do
   describe "Applicative Parser" $ do
     describe "pure" $ do
       it "creates a parser that consumes nothing and returns a value" $ do
-        pending
-        -- property $ \str -> runParser (pure ()) str == Just ((), str)
+        --pending
+        property $ \str -> runParser (pure ()) str == Just ((), str)
 
     describe "<*>" $ do
       it "applies a function from a parser to the result of a parser" $ do
-        pending
+        --pending
         let p1 = pure (+1) <*> posInt
         runParser p1 "41" `shouldBe` Just (42, "")
 
@@ -42,21 +42,21 @@ spec = do
 
   describe "abParser" $ do
     it "parses the characters 'a' and 'b' as a pair" $ do
-      pending
+      --pending
       runParser abParser "abcdef" `shouldBe` Just (('a', 'b'), "cdef")
       runParser abParser "bcdefa" `shouldBe` Nothing
       runParser abParser "aecdbf" `shouldBe` Nothing
 
   describe "abParser_" $ do
     it "parses the characters 'a' and 'b' but returns nothing" $ do
-      pending
+      --pending
       runParser abParser_ "abcdef" `shouldBe` Just ((), "cdef")
       runParser abParser_ "bcdefa" `shouldBe` Nothing
       runParser abParser_ "aecdbf" `shouldBe` Nothing
 
   describe "intPair" $ do
     it "parses two integer values separated by a space" $ do
-      pending
+      --pending
       runParser intPair "12 34" `shouldBe` Just ([12, 34], "")
 
   describe "Alternative Parser" $ do
@@ -75,7 +75,7 @@ spec = do
         runParser p2 "1234" `shouldBe` Just ('1', "234")
 
       it "user the second parser if the first one fails" $ do
-        pending
+        --pending
         let p1 = char '*' <|> char '$'
         runParser p1 "$abc" `shouldBe` Just ('$', "abc")
 
@@ -88,7 +88,7 @@ spec = do
 
   describe "intOrUppercase" $ do
     it "consumes an integer or an uppercase character" $ do
-      pending
+      --pending
       runParser intOrUppercase "342abcd" `shouldBe` Just ((), "abcd")
       runParser intOrUppercase "XYZ" `shouldBe` Just ((), "YZ")
       runParser intOrUppercase "foo" `shouldBe` Nothing
