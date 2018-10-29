8ac0027feffdfb40ba09e450a9b4bfd07b6f7715
Solomon Bothwell <ssbothwell@gmail.com>
2018-10-28 17:37:59 -0700

    Added week7 quickCheck tests

--- src/Homework/Week07/JoinList.hs
+++ src/Homework/Week07/JoinList.hs
@@ -19,9 +19,11 @@ import Homework.Week07.Scrabble
 import Homework.Week07.Sized
 import Data.Monoid
 
-data JoinList m a = Empty
-                  | Single m a
-                  | Append m (JoinList m a) (JoinList m a) deriving (Eq, Show)
+data JoinList m a = 
+      Empty
+    | Single m a
+    | Append m (JoinList m a) (JoinList m a)
+    deriving (Eq, Show)
 
 tag :: Monoid m => JoinList m a -> m
 tag Empty           = mempty
@@ -31,10 +33,10 @@ tag (Append m x x') = m
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) Empty x = x
 (+++) x Empty = x
-(+++) (Single m a) (Single m' a') = Append (m <> m') (Single m a) (Single m' a')
+(+++) (Single m a) (Single m' a')      = Append (m <> m') (Single m a) (Single m' a')
 (+++) (Append m l r) (Append m' l' r') = Append (m <> m') (Append m l r) (Append m' l' r')
-(+++) (Append m l r) (Single m' a) = Append (m <> m') (Append m l r) (Single m' a)
-(+++) (Single m' a) (Append m l r) = Append (m <> m') (Single m' a) (Append m l r)
+(+++) (Append m l r) (Single m' a)     = Append (m <> m') (Append m l r) (Single m' a)
+(+++) (Single m' a) (Append m l r)     = Append (m <> m') (Single m' a) (Append m l r)
 
 jlToList :: Monoid m => JoinList m a -> [a]
 jlToList Empty          = []
@@ -44,8 +46,8 @@ jlToList (Append m l r) = jlToList l <> jlToList r
 (!!?) :: [a] -> Int -> Maybe a
 [] !!? _         = Nothing
 _  !!? i | i < 0 = Nothing
-(x:xs)  !!? 0    = Just x
-(x:xs)  !!? i    = xs !!? (i-1)
+(x:xs) !!? 0     = Just x
+(x:xs) !!? i     = xs !!? (i-1)
 
 gss :: Sized a => a -> Int
 gss = getSize . size
@@ -54,7 +56,9 @@ gsst :: (Monoid b, Sized b) => JoinList b a -> Int
 gsst = getSize . size . tag
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ i (Single m a) = Just a
+indexJ i (Single m a) 
+    | i == 0 = Just a
+    | otherwise = Nothing
 indexJ i (Append m l r)
     | i < gsst l = indexJ i l
     | otherwise = indexJ (i - gsst l) r
@@ -62,40 +66,46 @@ indexJ _ _ = Nothing
 
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ 0 jl = jl
+dropJ 0 jl    = jl
 dropJ _ Empty = Empty
 dropJ i (Single m a)
     | i >= gss m = Empty
-    | otherwise = Single m a
+    | otherwise  = Single m a
 dropJ i (Append m l r)
-    | i >= gss m = Empty
+    | i >= gss m  = Empty
     | i == gsst l = r
-    | otherwise = (dropJ i l) +++ r
+    | i > gsst l  = dropJ (i - gsst l) r
+    | otherwise   = (dropJ i l) +++ r
 
 
 takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 takeJ _ Empty = Empty
 takeJ i (Append m l r)
-    | i >= gss m = Append m l r
+    | i >= gss m  = Append m l r
     | i == gsst l = l 
-    | i > gsst l = l +++ takeJ (i - gsst l) r
-    | otherwise = takeJ i l
+    | i > gsst l  = l +++ takeJ (i - gsst l) r
+    | otherwise   = takeJ i l
 takeJ i (Single m a)
     | i >= gss m = Single m a
     | gss m == i = Single m a
-    | otherwise = Empty
+    | otherwise  = Empty
 
 
 toStringJ :: JoinList (Score, Size) String -> String
-toStringJ Empty = ""
-toStringJ (Single m a) = a
-toStringJ (Append m l r) = toStringJ l <> "\n" <> toStringJ r
+toStringJ Empty          = ""
+toStringJ (Single m a)   = a <> "\n"
+toStringJ (Append m l r) = toStringJ l <> toStringJ r
 
 insertJ :: String -> JoinList (Score, Size) String
-insertJ str = f str Empty
-    where f [] jl = jl
-          f (x:xs) Empty = f xs (Single (score x, Size 1) [x])
-          f (x:xs) jl =  jl +++ (f xs (Single (score x, Size 1) [x]))
+insertJ ""   = Empty
+insertJ "\n" = Empty
+insertJ str  = foldl insertJ' Empty (lines str)
+
+insertJ' :: JoinList (Score, Size) String -> String -> JoinList (Score, Size) String
+insertJ' j str = f str j
+    where f [] jl    = jl
+          f xs Empty = Single (scoreString xs, Size 1) xs
+          f xs jl    =  jl +++ Single (scoreString xs, Size 1) xs
 
 scoreLine :: String -> JoinList Score String
 scoreLine xs = Single (scoreString xs) xs
@@ -111,9 +121,10 @@ replaceLineJ i str (Append m l r)
     | i < gsst l = replaceLineJ i str l +++ r
     | otherwise = l +++ replaceLineJ (i - gsst l) str r
 replaceLineJ i str (Single m a)
-    | i == gss m = Single (scoreString str, Size 1) str
     | i == 0 = Single (scoreString str, Size 1) str
-    | otherwise = Empty
+    | otherwise = Single m a
+--replaceLineJ 0 "" Empty = Empty
+--replaceLineJ 0 str Empty = Single (scoreString str, Size 1) str
 replaceLineJ _ _ Empty = Empty
 
 swapString :: String 
@@ -124,7 +135,7 @@ swapString str jl = jl
 
 instance Buffer (JoinList (Score, Size) String) where
   fromString :: String -> JoinList (Score, Size) String
-  fromString = insertJ . filter (/= '\n')
+  fromString = insertJ 
 
   line :: Int -> JoinList (Score, Size) String -> Maybe String
   line = indexJ
@@ -146,6 +157,6 @@ instance Buffer (JoinList (Score, Size) String) where
 
   value :: JoinList (Score, Size) String -> Int
   value t = case t of
-    Empty -> 0
-    (Single m _) -> getScore $ fst m
+    Empty          -> 0
+    (Single m _)   -> getScore $ fst m
     (Append m _ _) -> getScore $ fst m
--- test/Homework/Week07Spec.hs
+++ test/Homework/Week07Spec.hs
@@ -6,6 +6,7 @@ module Homework.Week07Spec (
 
 import Test.Hspec
 import Test.QuickCheck
+import Test.QuickCheck.Gen
 
 import Homework.Week07.Sized
 import Homework.Week07.JoinList
@@ -13,9 +14,53 @@ import Homework.Week07.Scrabble
 import Homework.Week07.Buffer
 
 import Data.Monoid
+import Data.Foldable (fold)
 
 main :: IO ()
 main = hspec spec
+
+instance Arbitrary Size where
+    arbitrary = sizeGen
+
+sizeGen :: Gen Size
+sizeGen = pure $ Size 1
+
+instance ( Arbitrary m
+         , Arbitrary a
+         , Monoid m ) => Arbitrary (JoinList m a) where
+    arbitrary = sized joinListGen
+
+joinListGen :: (Arbitrary m, Arbitrary a, Monoid m) 
+            => Int -> Gen (JoinList m a)
+joinListGen depth 
+    | depth == 0 = Single <$> arbitrary <*> arbitrary
+    | depth > 0 = 
+        oneof
+            [ Single <$> arbitrary <*> arbitrary
+            , (+++) <$> (joinListGen (depth - 1)) <*> (joinListGen (depth - 1))]
+
+genSafeChar :: Gen Char
+genSafeChar = elements ['a'..'z']
+
+genSafeString :: Gen String
+genSafeString = listOf genSafeChar
+
+genSafeStrings :: Gen String
+genSafeStrings = fmap foldStrings $ listOf genSafeString
+
+genScopedInt :: (Int -> Bool) -> Gen Int
+genScopedInt f = suchThat (arbitrary :: Gen Int) f
+
+foldStrings :: [String] -> String
+foldStrings = 
+    let f a b = if a /= "" then a ++ "\n" ++ b else b
+    in foldr f ""
+
+-- Naive tree traversal size count.
+sizeJ :: JoinList m a -> Int
+sizeJ Empty = 0
+sizeJ (Single _ _) = 1
+sizeJ (Append _ l r) = sizeJ l + sizeJ r
 
 spec :: Spec
 spec = do
@@ -26,6 +71,13 @@ spec = do
       tag (Single (Sum 10) 'a') `shouldBe` Sum 10
 
   describe "+++" $ do
+    it "appended joinlist should always `mappend` child annotations" $ do
+      property $
+          \jl1 jl2 -> 
+              let m1 = (getSize . size . tag) (jl1 :: JoinList Size Char)
+                  m2 = (getSize . size . tag) (jl2 :: JoinList Size Char)
+                  m3 = (getSize . size . tag) $ (+++) jl1 jl2
+              in m1 + m2 == m3
     it "appends two JoinList structures together" $ do
       --pending
       let a = Single (Sum 3) 'a'
@@ -35,9 +87,27 @@ spec = do
   describe "exercise 2" $ do
     let jl = Append (Size 3) (Append (Size 2) (Single (Size 1) 's') (Single (Size 1) 'u')) (Single (Size 1) 'p')
 
-    let jl2 = Append (Size 4) (Append (Size 2) (Single (Size 1) 'a') (Single (Size 1) 'b')) (Append (Size 2) (Single (Size 1) 'c') (Single (Size 1) 'd')) 
+    describe "quickCheck" $ do
+      it "indexJ should always behave the same as ((!!?) . jlToList)" $ do
+        --pending
+        property $ 
+          \jl i -> 
+            (==) (indexJ i jl) 
+                 ((!!?) (jlToList (jl :: JoinList Size Char)) (i :: Int))
 
-    let jl3 = (Append (Size 5) (Append (Size 2) (Single (Size 1) 'a') (Single (Size 1) 'b')) (Append (Size 3) (Single (Size 1) 'c') (Append (Size 2) (Single (Size 1) 'd') (Single (Size 1) 'e')) ))
+      it "dropJ should always behave the same as (drop i $ jlToList)" $ do
+        --pending
+        property $ 
+          \jl i -> 
+            (==) (jlToList $ dropJ i jl) 
+                 (drop (i :: Int) (jlToList (jl :: JoinList Size Char)))
+
+      it "takeJ should always behave the same as (take i $ jlToList)" $ do
+        --pending
+        property $ 
+          \jl i -> 
+            (==) (jlToList $ takeJ i jl) 
+                 (take (i :: Int) (jlToList (jl :: JoinList Size Char)))
 
     describe "indexJ" $ do
       it "finds a letter at the specified index" $ do
@@ -45,32 +115,6 @@ spec = do
         indexJ 0 jl `shouldBe` Just 's'
         indexJ 1 jl `shouldBe` Just 'u'
         indexJ 2 jl `shouldBe` Just 'p'
-
-    describe "indexJ2" $ do
-      it "finds a letter at the specified index" $ do
-        --pending
-        indexJ 0 jl2 `shouldBe` Just 'a'
-        indexJ 1 jl2 `shouldBe` Just 'b'
-        indexJ 2 jl2 `shouldBe` Just 'c'
-        indexJ 3 jl2 `shouldBe` Just 'd'
-
-    describe "indexJ3" $ do
-      it "finds a letter at the specified index" $ do
-        --pending
-        (!!?) (jlToList jl3) 0 `shouldBe` Just 'a'
-        (!!?) (jlToList jl3) 1 `shouldBe` Just 'b'
-        (!!?) (jlToList jl3) 2 `shouldBe` Just 'c'
-        (!!?) (jlToList jl3) 3 `shouldBe` Just 'd'
-        (!!?) (jlToList jl3) 4 `shouldBe` Just 'e'
-
-    describe "indexJ3" $ do
-      it "finds a letter at the specified index" $ do
-        --pending
-        indexJ 0 jl3 `shouldBe` Just 'a'
-        indexJ 1 jl3 `shouldBe` Just 'b'
-        indexJ 2 jl3 `shouldBe` Just 'c'
-        indexJ 3 jl3 `shouldBe` Just 'd'
-        indexJ 4 jl3 `shouldBe` Just 'e'
 
     describe "dropJ" $ do
       it "returns the list when dropping 0" $ do
@@ -83,6 +127,7 @@ spec = do
         --pending
         jlToList (dropJ 2 jl) `shouldBe` ['p']
 
+
     describe "takeJ" $ do
       it "returns the list when taking more than the list" $ do
         --pending
@@ -93,9 +138,6 @@ spec = do
       it "takes the first n elements from a JoinList" $ do
         --pending
         jlToList (takeJ 2 jl) `shouldBe` ['s', 'u']
-      it "takes the first n elements from a JoinList2" $ do
-        --pending
-        jlToList (takeJ 2 jl3) `shouldBe` ['a', 'b']
 
   describe "scoreLine" $ do
     it "scores a single word" $ do
@@ -115,18 +157,99 @@ spec = do
     let testString = unlines ["a", "z"]
     let testJoinList = Append (Score 11, Size 2)
           (Single (Score 1, Size 1) "a")
-          (Append (Score 10, Size 1) (Single (Score 10, Size 1) "z") Empty)
+          (Single (Score 10, Size 1) "z")
           :: JoinList (Score, Size) String
+
+    describe "quickCheck" $ do
+      it "`toString . fromString` is `identity :: String -> String`" $ do
+        --pending
+        property $ 
+          forAll genSafeStrings $
+          \xs -> 
+            let identity ys = toString (fromString ys :: JoinList (Score, Size) String)
+            in identity xs == xs
+
+      it "`fromString . toString` is `identity :: JoinList m a -> JoinList m a`" $ do
+        --pending
+        property $ 
+          forAll genSafeStrings $
+          \xs -> 
+            let jl = fromString xs :: JoinList (Score, Size) String
+                identity jl' = fromString (toString jl') :: JoinList (Score, Size) String
+            in identity jl == jl
+
+      it "numLines gives same result as length after filtering non-newlines" $ do
+        --pending
+        property $ 
+          forAll genSafeStrings $
+          \xs -> 
+            let jl = fromString xs :: JoinList (Score, Size) String
+            in (numLines jl) == (length . filter (\x -> x == '\n')) xs
+
+      it "replaceLine adjusts score appropriately" $ do
+        --pending
+        property $ 
+          forAll (suchThat genSafeStrings (\x -> x /= "")) $ \xs -> 
+          forAll genSafeString $ \str -> 
+            let jl = fromString xs :: JoinList (Score, Size) String
+                sizeJl = numLines jl
+                pred   = \x -> (x >= 0 && x < sizeJl) || (x == 0 && sizeJl == 0)
+            in forAll (genScopedInt pred) $ \i ->
+              let (Score s)  = scoreString . fold $ (line i jl)
+                  jl2        = replaceLine i str jl
+                  (Score s') = scoreString str
+              in value jl2 == (value jl - s) + s'
+
+      it "`line i` after `replaceLine i str` returns `str`" $ do
+        --pending
+        property $ 
+          forAll (suchThat genSafeStrings (\x -> x /= "")) $ \xs -> 
+          forAll genSafeString $ \str -> 
+            let jl     = fromString xs :: JoinList (Score, Size) String
+                sizeJl = numLines jl
+                pred   = \x -> (x >= 0 && x < sizeJl) || (x == 0 && sizeJl == 0)
+            in forAll (genScopedInt pred) $ \i ->
+              let jl2 = replaceLine i str jl
+              in (fold $ line i jl2) == str
+
+      it "`replaceLine i str` is idempotent" $ do
+        --pending
+        property $ 
+          forAll genSafeStrings $ \xs -> 
+          forAll genSafeString $ \str -> 
+            let jl = fromString xs :: JoinList (Score, Size) String
+                sizeJl = numLines jl
+            in forAll (genScopedInt (\x -> (x >= 0 && x < sizeJl) || x == 0 && sizeJl == 0)) $ \i ->
+              let jl2 = replaceLine i str jl
+                  jl3 = replaceLine i str jl2
+              in jl2 == jl3
+
+      it "replaceLine with a new line then replaceLine with the original line \
+         \ gives the original JoinList" $ do
+        --pending
+        property $ 
+          forAll genSafeStrings $ \xs -> 
+          forAll genSafeString $ \str -> 
+            let jl = fromString xs :: JoinList (Score, Size) String
+                sizeJl = numLines jl
+            in forAll (genScopedInt (\x -> (x >= 0 && x < sizeJl) || x == 0 && sizeJl == 0)) $ \i ->
+              let jl2 = replaceLine i str jl
+                  str' = fold $ line i jl
+                  jl3 = replaceLine i str' jl2
+              in jl3 == jl
+
     describe "fromString" $ do
       it "converts a string to a JoinList" $ do
         --pending
         let processedList = fromString testString :: JoinList (Score, Size) String
         jlToList processedList `shouldBe` jlToList testJoinList
         tag processedList `shouldBe` (Score 11, Size 2)
+
     describe "toString" $ do
       it "converts a JoinList to a string" $ do
         --pending
         toString testJoinList `shouldBe` testString
+
     describe "line" $ do
       it "gets a single line" $ do
         --pending
@@ -137,10 +260,12 @@ spec = do
       it "gets a second line from a joinList" $ do
         --pending
         line 1 testJoinList `shouldBe` Just "z"
+
     describe "numLines" $ do
-      it "counts lines" $ do
+      it "counts lines" $ do 
         --pending
         numLines testJoinList `shouldBe` 2
+
     describe "replaceLine" $ do
       it "replaces the first line" $ do
         --pending
